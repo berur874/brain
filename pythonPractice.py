7bd6416ed57d1a88635c2d896734ce0fb26ecc3f
markroy <markroyberur@gmail.com>
2026-07-06 16:12:32 +0300

    More Updates

--- pythonPractice.py
+++ pythonPractice.py
@@ -191,3 +191,121 @@ def inOrderTraversal(node):
     inOrderTraversal(node.right)# finally visits the right subtree
 
 inOrderTraversal(root)
+print("\n")
+
+# POST-ORDER TRAVERSAL OF BINARY TREES --> It works by recursively doing a post order of the left subtree and the right subtree, followed by a visit to the root node.
+def postOrderTrasversal(node):
+  if node is None: # base case
+    return
+  postOrderTrasversal(node.left) # visits the left node
+  postOrderTrasversal(node.right) # visits the right node
+  print(node.data, end=",") # finallly visits the node
+
+postOrderTrasversal(root) 
+print("\n") 
+
+""" 
+ BINARY SEARCH TRESS --> It is a type of binary tree data structure, where the following properties must be true for any node "X" in the tree
+  1) The X node's left child and all of its decendants have lower
+     values than X
+  2) The right child, and all its decendants have higher values than X
+  3) Left and right subtrees must also be Binary Search Trees.    
+"""
+
+# TRAVERSAL OF A BINARY SEARCH TREE
+
+class TreeNode:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None 
+
+def inOrderTrasversal(node):
+    if node is None:
+        return
+    inOrderTrasversal(node.left)
+    print(node.info, end=",")
+    inOrderTrasversal(node.right)
+
+# searching for a node in BST
+def search(node,target):
+    if target is None:
+        return None
+    elif node.info == target:
+        return node
+    elif target < node.info:
+        return search(node.left, target)
+    else:
+        return search(node.right, target)
+# Inserting a new node in a BST
+def insertNode(node, info):
+    if node is None:
+        return TreeNode(info)
+    else:
+        if info < node.info:
+            node.left = insertNode(node.left, info)
+        elif info > node.info:
+            node.right = insertNode(node.right, info) 
+    return node 
+# finding lowest value in a BST(left subtree)
+def lowestNode(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current               
+# deleting a node in BST
+def deleteNode(node, info):
+    if not node:
+        return None # makes the function to be able to call itself recursively
+    
+    if info < node.info:
+        node.left = deleteNode(node.left, info) # if the value to be deleted is smaller than the current node's value, go to the left subtree
+    elif info > node.info:
+        node.right = deleteNode(node.right, info) # if the value to be deleted is larger than the current node value, go right
+    else: # we found the node to delete
+        #Node with only one child or no child
+         if not node.left: # if no left child
+             temp = node.right # save the right child
+             node = None # delete the current node and set to none
+             return temp # return the right child to replace this node
+         elif not node.right: # if no right child
+             temp = node.left # save the left child
+             node = None # delete the current node
+             return temp # return the left child to replace the deleted node
+
+        #Node with two children, get the in-order successor
+         node.info = lowestNode(node.right).info # find the in-order successor & copy its data to the current node
+         node.right = deleteNode(node.right, node.info) # recursively delete the succesor value from the right subtree and update the root
+    return node     
+
+
+root = TreeNode(13)
+node7 = TreeNode(7)
+node15 = TreeNode(15)
+node3 = TreeNode(3)
+node8 = TreeNode(8)
+node14 = TreeNode(14) 
+node19 =TreeNode(19)
+node18 = TreeNode(18)
+
+root.left = node7
+root.right = node15
+
+node7.left = node3
+node7.right = node8
+    
+node15.left = node14
+node15.right = node19
+
+node19.left = node18
+deleteNode(root, 15)
+insertNode(root, 10)
+inOrderTrasversal(root,)
+print("\n") 
+result = search(root, 13)
+if result:
+    print(f"Found the node with value: {result.info}")
+else:
+    print("Value not found in the BST") 
+
+print("\nLowest value:", lowestNode(root).info)       
